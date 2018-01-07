3ec8d775290c232c76b131cbe1bd8a38fa56f628
CtrlNull <raphael@fonseca.tech>
2018-01-07 08:45:48 -0800

    updated components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { AppService} from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  greetings = 'test my app';
+  title = 'Crud App using Angular Cli';
+
+  constructor(private _appService: AppService) { }
+
+  ngOnInit(): void {
+    this._appService.sayHello()
+    .subscribe(
+      result => {
+        this.greetings = result;
+      }
+    );
+  }
+
+
+
+
 }
